69ef34a45ce4f567fbebc1658541c671ffafe51f
Art Mayes <artmayes167@gmail.com>
2012-04-11 13:42:06 -0500

    Initial Commit

---
+++ SkyAlch_iPhone/AEMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AEMAppDelegate.h
+//  SkyAlch_iPhone
+//
+//  Created by Arthur Mayes on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AEMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
